feece59001daf829dbe9da8721c517e886abb9f8
Filip785 <fdjuricic98@gmail.com>
2021-02-02 23:43:47 +0100

    Add creation/dropping of products table to seed.

--- install/seed.php
+++ install/seed.php
@@ -8,6 +8,32 @@ $dir = dirname(__DIR__);
 require $dir . '/vendor/autoload.php';
 
 DatabaseLoader::loadDB($dir . '/src/config/database.php');
+
+$pdoInstance = DB::getInstance();
+
+$tableDrop = $pdoInstance->prepare('DROP TABLE IF EXISTS `products`');
+$tableDrop->execute();
+
+if($tableDrop->execute()) {
+    echo 'Products table dropped.' . PHP_EOL;
+} else {
+    echo 'Failed to drop procuts table.' . PHP_EOL;
+}
+
+$tableCreation = $pdoInstance->prepare('CREATE TABLE `products` (
+    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `image` varchar(255) NOT NULL
+  )
+');
+
+if($tableCreation->execute()) {
+    echo 'Products table created.' . PHP_EOL;
+} else {
+    echo 'Failed to create products table.' . PHP_EOL;
+}
+
 DB::table('products')->create(['image' => '/assets/images/citrus1.jpg', 'title' => 'Citrus Juice 1', 'description' => 'Very nice citrus juice 1!']);
 DB::table('products')->create(['image' => '/assets/images/citrus2.jpeg', 'title' => 'Citrus Juice 2', 'description' => 'Very nice citrus juice2 !']);
 DB::table('products')->create(['image' => '/assets/images/citrus3.jpg', 'title' => 'Citrus Juice 3', 'description' => 'Very nice citrus juice! 3']);

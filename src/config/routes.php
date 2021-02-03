300db5d2376f6739d9a916880b2f13a122faed17
Filip785 <fdjuricic98@gmail.com>
2021-02-03 02:27:58 +0100

    Refactor routing outside routes.php into loader file

--- src/config/routes.php
+++ src/config/routes.php
@@ -1,9 +1,8 @@
 <?php
 
-use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 
-$dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
+return FastRoute\simpleDispatcher(function (RouteCollector $r) {
     $r->addRoute('GET', '/', 'HomeController@index');
 
     $r->addRoute('GET', '/dashboard', 'DashboardController@index');
@@ -11,67 +10,10 @@ $dispatcher = FastRoute\simpleDispatcher(function (RouteCollector $r) {
     $r->addRoute('GET', '/dashboard/logout', 'DashboardController@logout');
     $r->addRoute('POST', '/dashboard/doLogin', 'DashboardController@doLogin');
 
+    $r->addRoute('GET', '/products', 'ProductsController@index');
+    $r->addRoute('GET', '/products/{product_id:\d+}', 'ProductsController@get');
+
     $r->addRoute('POST', '/comments/create', 'CommentsController@create');
     $r->addRoute('POST', '/comments/allow/{comment_id:\d+}', 'CommentsController@allow');
     $r->addRoute('POST', '/comments/disallow/{comment_id:\d+}', 'CommentsController@disallow');
-
-    // list all products
-    $r->addRoute('GET', '/products', 'ProductsController@index');
-    // get single product
-    $r->addRoute('GET', '/products/{product_id:\d+}', 'ProductsController@get');
 });
-
-// Fetch method and URI from somewhere
-$httpMethod = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
-
-// Strip query string (?foo=bar) and decode URI
-if (false !== $pos = strpos($uri, '?')) {
-    $uri = substr($uri, 0, $pos);
-}
-
-$uri = rawurldecode($uri);
-$controllersPath = dirname(__DIR__) . '../Controllers/';
-$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
-switch ($routeInfo[0]) {
-    case Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
-        // TODO: HANDLE NOT FOUND
-        echo 'not found';
-
-        break;
-    case Dispatcher::METHOD_NOT_ALLOWED:
-        $allowedMethods = $routeInfo[1];
-        // ... 405 Method not allowed
-        // TODO: HANDLE METHOD NOT ALLOWED
-        echo 'method not allowed';
-
-
-        break;
-    case Dispatcher::FOUND:
-        $handler = $routeInfo[1];
-        $vars = $routeInfo[2];
-
-        $handlerParams = explode('@', $handler);
-
-        if (!isset($handlerParams[0]) || !isset($handlerParams[1])) {
-            dd('Please enter your action pattern in the following format: SomeController@index.');
-        }
-
-        $className = $handlerParams[0];
-        $fullNamespaceName = "Filip785\\MVC\\Controllers\\" . $className;
-
-        if (!class_exists($fullNamespaceName)) {
-            dd('Your controller class does not exist. Please provide it in src/Controllers/ and use the following namespace to define it in: Filip785\\MVC\\Controllers.');
-        }
-
-        $actionMethod = $handlerParams[1];
-        if (!method_exists($fullNamespaceName, $actionMethod)) {
-            dd("Action '$actionMethod' does not exist in $fullNamespaceName. Please create it.");
-        }
-
-        $class = new $fullNamespaceName($className, $actionMethod);
-        $class->$actionMethod(...$vars);
-
-        break;
-}
--- src/loaders/RoutingLoader.php
+++ src/loaders/RoutingLoader.php
@@ -2,7 +2,8 @@
 
 namespace Filip785\MVC\Loaders;
 
-// Loads route file.
+use FastRoute\Dispatcher;
+
 class RoutingLoader
 {
     public static function loadMVCRoutes(string $routesPath)
@@ -11,6 +12,65 @@ class RoutingLoader
             dd("Routes file does not exist. There needs to be routes file in $routesPath.");
         }
 
-        include dirname(__DIR__) . '../config/routes.php';
+        $dispatcher = include dirname(__DIR__) . '../config/routes.php';
+
+        self::bootstrap($dispatcher);
+    }
+
+    public static function bootstrap($dispatcher)
+    {
+        // Fetch method and URI from somewhere
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+
+        // Strip query string (?foo=bar) and decode URI
+        if (false !== $pos = strpos($uri, '?')) {
+            $uri = substr($uri, 0, $pos);
+        }
+
+        $uri = rawurldecode($uri);
+        $controllersPath = dirname(__DIR__) . '../Controllers/';
+
+        $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+        switch ($routeInfo[0]) {
+            case Dispatcher::NOT_FOUND:
+                // ... 404 Not Found
+                echo '<h1>not found</h1>';
+
+                break;
+            case Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                // ... 405 Method not allowed
+                echo '<h1>method not allowed</h1>';
+
+                break;
+            case Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+
+                $handlerParams = explode('@', $handler);
+
+                if (!isset($handlerParams[0]) || !isset($handlerParams[1])) {
+                    dd('Please enter your action pattern in the following format: SomeController@index.');
+                }
+
+                $className = $handlerParams[0];
+                $fullNamespaceName = "Filip785\\MVC\\Controllers\\" . $className;
+
+                if (!class_exists($fullNamespaceName)) {
+                    dd('Your controller class does not exist. Please provide it in src/Controllers/ and use the following namespace to define it in: Filip785\\MVC\\Controllers.');
+                }
+
+                $actionMethod = $handlerParams[1];
+                if (!method_exists($fullNamespaceName, $actionMethod)) {
+                    dd("Action '$actionMethod' does not exist in $fullNamespaceName. Please create it.");
+                }
+
+                $class = new $fullNamespaceName($className, $actionMethod);
+                $class->$actionMethod(...$vars);
+
+                break;
+        }
     }
 }

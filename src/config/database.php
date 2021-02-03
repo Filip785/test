915bc4d981e1377e08e70683bb34f9c15033cb3c
Filip785 <fdjuricic98@gmail.com>
2021-02-03 11:20:58 +0100

    Ignore database.php

--- src/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-# file that will be used for providing connection parameters to the database
-return [
-    'username' => 'root',
-    'password' => '',
-    'db_host' => 'localhost',
-    'db_name' => 'citrus'
-];
